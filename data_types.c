dba5c4727260b88878e65d48a02fa627f23aba68
Vinodsmiley8 <78037224+Vinodsmiley8@users.noreply.github.com>
2022-12-08 22:46:16 +0530

    Add files via upload

---
+++ data_types.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>  
+ int main()    
+{    
+
+	int a,b,c;
+	float d,e,f;
+	char g,h;
+	
+	a=1;   //int
+	d=9.998; //float
+	g='z';//char
+	b=(int)d;//int
+	e=(float)a;//float
+	c=(int)g;//int
+	f=(float)g;//float
+	
+	printf(" %d\n %d\n %d\n %f\n %f\n %f\n %c\n %c\n ",a,b,c,d,e,f,g,h);
+	
+return 0;  
+}
